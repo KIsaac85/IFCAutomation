631e39d42d877bcb7909d473ba7fe8dfdfae4bfe
Thinkpad <karim_isaac@hotmail.com>
2025-10-11 14:06:28 +0200

    initial commit

---
+++ IFC Parameter Rev01/IFC Parameter.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+
+
+namespace IFC_Parameter_Automation
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class IFCParameters : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Get the current document
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            // Your code to automate IFC parameters goes here
+
+            UserControl1 win = new UserControl1();
+            win.ShowDialog();
+            return Result.Succeeded;
+        }
+    }
+
+
+}
---
+++ IFC Parameter Rev01/UserControl1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IFC_Parameter_Automation
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : Window
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void Column_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Wall_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
